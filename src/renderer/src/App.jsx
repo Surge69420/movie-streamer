59f914ad1f97411b184bb17f48208682b5dcb67d
Surge69420 <stormbreakerxtno1@gmail.com>
2024-04-13 16:12:30 +0100

    renders the movies and their names

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -1,15 +1,25 @@
 import { useState, useEffect } from 'react'
 function App() {
-  
-  const [movies, setmovies] = useState({});
-  
+
+  const [movies, setmovies] = useState(
+    [
+      {
+        "i": {
+          "imageUrl": "https://m.media-amazon.com/images/M/MV5BN2IzYzBiOTQtNGZmMi00NDI5LTgxMzMtN2EzZjA1NjhlOGMxXkEyXkFqcGdeQXVyNjAwNDUxODI@._V1_.jpg"
+        },
+        "l": "Game of Thrones",
+      }
+    ]
+  );
+
   useEffect(() => {
     // Sending 'ping' message when component mounts
     window.electron.ipcRenderer.send('ping');
 
     // Setting up event listener to receive 'pong' message
     window.electron.ipcRenderer.on("pong", (event, message) => {
-      setmovies(JSON.parse(message));
+      let data = JSON.parse(message);
+      setmovies(data.d);
     });
 
     // Cleanup function to remove event listener when component unmounts
@@ -18,9 +28,22 @@ function App() {
     };
   }, []);
   console.log(movies)
+  const extractedData = movies.map(movie => {
+    return {
+        imageUrl: movie.i ? movie.i.imageUrl : '',
+        title: movie.l
+    };
+});
   return (
     <>
-
+      <div>
+        {extractedData.map((movie, index) => (
+          <div key={index}>
+            <img src={movie.imageUrl} alt={movie.l} style={{ width: '100px' }} />
+            <p>{movie.title}</p>
+          </div>
+        ))}
+      </div>
     </>
   )
 }
